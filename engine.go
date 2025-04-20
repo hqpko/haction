a4d5812a3b1928f0b091e1abc364df565fb84216
hqpko <whaibin01@126.com>
2025-04-20 21:38:48 +0800

    fix: get engine handler concurrency

--- engine.go
+++ engine.go
@@ -1,6 +1,9 @@
 package haction
 
+import "sync"
+
 type Engine struct {
+	lock sync.RWMutex
 	*group
 	handlers map[int32][]HandleAction
 }
@@ -12,13 +15,21 @@ func NewEngine() *Engine {
 }
 
 func (e *Engine) register(pid int32, handlers []HandleAction) {
+	e.lock.Lock()
+	defer e.lock.Unlock()
 	e.handlers[pid] = handlers
 }
 
 func (e *Engine) Do(id int32, values Values) {
-	newContext(id, values, e.handlers[id]).do()
+	newContext(id, values, e.getHandler(id)).do()
 }
 
 func (e *Engine) newContext(id int32, values Values) *Context {
-	return newContext(id, values, e.handlers[id])
+	return newContext(id, values, e.getHandler(id))
+}
+
+func (e *Engine) getHandler(id int32) []HandleAction {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	return e.handlers[id]
 }
